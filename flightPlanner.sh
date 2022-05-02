a76701a14aee0884261c25eb932c0d6733502160
Jakub <czaplickijakub1@gmail.com>
2022-05-02 17:50:16 -0400

    Added runway array

--- flightPlanner.sh
+++ flightPlanner.sh
@@ -1,21 +1,50 @@
-# Jakub Czaplicki
+#!/bin/bash
+
+# Claude Batrouni & Jakub Czaplicki
 # May 11 2022
 # Flight Planning Software Solution
 
 NOW=$(date +%b' '%d)
+declare -a runwayList
+$flag=true
 
 echo "Hello! Thank you for using this flight planning software. Today is ${NOW}."
 
 echo "Please enter the airport you are departing: "
 read DEPART
 
-echo "Please enter the airport you are arriving at: "
-read ARRIVE
+# echo "Please enter the airport you are arriving at: "
+# read ARRIVE
 
 echo "--------------------------------------------------------------------------------------------------"
 
 echo "You are departing from `curl -s -H "Accept: application/xml" https://api.flightplandatabase.com/nav/airport/${DEPART} | grep -oP '(?<=<name>).*?(?=</name>)' | head -1`. "
 
-echo "There are `curl -s -H "Accept: application/xml" https://api.flightplandatabase.com/nav/airport/${DEPART}| grep -oP '(?<=<runwayCount>).*?(?=</runwayCount>)'` runways available"
+echo "Please tune into the departure frequency at `curl -s -H "Accept: application/xml" https://api.flightplandatabase.com/nav/airport/${DEPART} | grep -oP '(?<=<frequency>).*?(?=</frequency>)' | head -22 | tail -1`"
 
-echo "Please tune into the departure frequency at `curl -s -H "Accept: application/xml" https://api.flightplandatabase.com/nav/airport/KJFK | grep -oP '(?<=<frequency>).*?(?=</frequency>)' | head -22 | tail -1`"
+runwayList=()
+while IFS= read -r line; do
+    runwayList+=( "$line" )
+done < <( curl -s -H "Accept: application/xml" https://api.flightplandatabase.com/nav/airport/KJFK | grep -oP '(?<=<runway>).*?(?=</runway>)' | uniq )
+
+RUNAMT=${#runwayList[@]}
+
+echo "There are `curl -s -H "Accept: application/xml" https://api.flightplandatabase.com/nav/airport/${DEPART}| grep -oP '(?<=<runwayCount>).*?(?=</runwayCount>)'` takeoff platforms available and ${RUNAMT} runways"
+
+echo "The runways that are available are: ${runwayList[*]}"
+
+echo "Please select your departing runway:"
+read RUNWAY
+
+for i in "${runwayList}";
+do
+    if [ "${i}" = RUNWAY ]
+    then
+        $flag=false
+    fi
+done
+
+if [ $flag=true ]
+then
+    echo "Not a valid runway"
+fi
