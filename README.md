9c39b1cf02778048019864835432ac739eed0242
Jakub <czaplickijakub1@gmail.com>
2022-05-05 14:47:01 -0400

    Polished up

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # flightPlanner
-A simple script for gathering info on airports
+## A script for gathering info on airports
+
+## USAGE: bash planner.sh
+
+YOu must use a valid ICAO Airport Code!
+See more [here](https://www.world-airport-codes.com/)
--- new.sh
+++ new.sh
@@ -8,22 +8,27 @@ NOW=$(date +%b' '%d)
 declare -a runwayList
 FLAG=1
 
+#the api used: https://flightplandatabase.com/dev/api#endpoint-weather
 #command to use: 
 #`curl -s -H "Accept: application/xml" https://api.flightplandatabase.com/nav/airport/${DEPART} | xmllint --xpath "/response/airport/name/text()" -`
 # use this website to get path locations: https://xmltoolbox.appspot.com/xpath_generator.html
 
 echo "Hello! Thank you for using this flight planning software. Today is ${NOW}."
+
 echo "Please enter the airport you are departing: "
 read DEPART
 
+echo "Please enter the airport you are arriving at: "
+read ARRIVE
+
+echo ""
+echo "--------------------------------------------------------------------------------------------------"
+echo "DEPARTURE"
 echo "--------------------------------------------------------------------------------------------------"
 
-echo "You are departing from `curl -s -H "Accept: application/xml" https://api.flightplandatabase.com/nav/airport/${DEPART} | xmllint --xpath "/response/airport/name/text()" -` Airport"
+echo "You are departing from `curl -s -H "Accept: application/xml" https://api.flightplandatabase.com/nav/airport/${DEPART} | xmllint --xpath "//*/airport/name/text()" -` Airport"
 
-# echo "Please enter the airport you are arriving at: "
-# read ARRIVE
-
-echo "Please contact the tower at `curl -s -H "Accept: application/xml" https://api.flightplandatabase.com/nav/airport/${DEPART} | xmllint --xpath "/response/airport/frequencies/frequency[15]/frequency/text()" -`" 
+echo "Please contact the tower at frequency `curl -s -H "Accept: application/xml" https://api.flightplandatabase.com/nav/airport/${DEPART} | xmllint --xpath "//*[type = 'DEP']/." - | grep -oP '(?<=<frequency>).*?(?=</frequency>)' | head -1`" 
 
 runwayList=()
 while IFS= read -r line; do
@@ -36,3 +41,21 @@ echo "There are `curl -s -H "Accept: application/xml" https://api.flightplandata
 
 echo "The runways that are available are: ${runwayList[*]}"
 
+echo "--------------------------------------------------------------------------------------------------"
+echo "ARRIVAL"
+echo "--------------------------------------------------------------------------------------------------"
+
+echo "You are arriving at `curl -s -H "Accept: application/xml" https://api.flightplandatabase.com/nav/airport/${ARRIVE} | xmllint --xpath "//*/airport/name/text()" -` Airport"
+
+echo "Please contact the tower at frequency `curl -s -H "Accept: application/xml" https://api.flightplandatabase.com/nav/airport/${ARRIVE} | xmllint --xpath "//*[type = 'APP']/." - | grep -oP '(?<=<frequency>).*?(?=</frequency>)' | head -1`" 
+
+runwayList=()
+while IFS= read -r line; do
+    runwayList+=( "$line" )
+done < <( curl -s -H "Accept: application/xml" https://api.flightplandatabase.com/nav/airport/${ARRIVE} | grep -oP '(?<=<runway>).*?(?=</runway>)' | uniq )
+
+RUNAMT=${#runwayList[@]}
+
+echo "There are `curl -s -H "Accept: application/xml" https://api.flightplandatabase.com/nav/airport/${ARRIVE} | xmllint --xpath "/response/airport/runwayCount/text()" -` take off platforms available and ${RUNAMT} runways"
+
+echo "The runways that are available are: ${runwayList[*]}"
