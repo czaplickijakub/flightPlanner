593222c7165ece94df7ec58c0e2a13ffbff61645
Jakub <czaplickijakub1@gmail.com>
2022-05-05 11:25:32 -0400

    Added loops

--- flightPlanner.sh
+++ flightPlanner.sh
@@ -5,11 +5,11 @@
 # Flight Planning Software Solution
 
 NOW=$(date +%b' '%d)
+URL=
 declare -a runwayList
-$flag=true
+$flag=1
 
 echo "Hello! Thank you for using this flight planning software. Today is ${NOW}."
-
 echo "Please enter the airport you are departing: "
 read DEPART
 
@@ -18,14 +18,14 @@ read DEPART
 
 echo "--------------------------------------------------------------------------------------------------"
 
-echo "You are departing from `curl -s -H "Accept: application/xml" https://api.flightplandatabase.com/nav/airport/${DEPART} | grep -oP '(?<=<name>).*?(?=</name>)' | head -1`. "
+echo "You are departing from `curl -s -H "Accept: application/xml" https://api.flightplandatabase.com/nav/airport/${DEPART} | grep -oP '(?<=<name>).*?(?=</name>)' | head -1`."
 
 echo "Please tune into the departure frequency at `curl -s -H "Accept: application/xml" https://api.flightplandatabase.com/nav/airport/${DEPART} | grep -oP '(?<=<frequency>).*?(?=</frequency>)' | head -22 | tail -1`"
 
 runwayList=()
 while IFS= read -r line; do
     runwayList+=( "$line" )
-done < <( curl -s -H "Accept: application/xml" https://api.flightplandatabase.com/nav/airport/KJFK | grep -oP '(?<=<runway>).*?(?=</runway>)' | uniq )
+done < <( curl -s -H "Accept: application/xml" https://api.flightplandatabase.com/nav/airport/${DEPART} | grep -oP '(?<=<runway>).*?(?=</runway>)' | uniq )
 
 RUNAMT=${#runwayList[@]}
 
@@ -36,15 +36,19 @@ echo "The runways that are available are: ${runwayList[*]}"
 echo "Please select your departing runway:"
 read RUNWAY
 
-for i in "${runwayList}";
+for i in "${runwayList[@]}"
 do
-    if [ "${i}" = RUNWAY ]
+    if [ "$i" = "$RUNWAY" ]
     then
-        $flag=false
+        flag=0
     fi
 done
 
-if [ $flag=true ]
+if [ $flag -eq 1 ]
 then
-    echo "Not a valid runway"
+    echo "Invalid Runway. Please input a valid runway."
+else
+    echo "Valid Runway!"
 fi
+#curl -s -H "Accept: application/xml" https://api.flightplandatabase.com/nav/airport/kjfk | sed 's/<runway>\(.*\)<\/runway>/\1/g'
+#xpath -q -e '//span[@id="yfs_l10_goog"][1]/text()'
